661807eab5273ccb1b8fd01d364da1787bfff31a
Will Huey <will.huey@gmail.com>
2012-02-10 20:54:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waidailo::Application.config.secret_token = '3a83c1a11649fa56b90e457b6e02681c1379770a93f2e26a26ed1cc05de8fbee8a1a47f3c861caa47ca88ef0aefc98c76867be12304d4cfc98eabeba51070796'
